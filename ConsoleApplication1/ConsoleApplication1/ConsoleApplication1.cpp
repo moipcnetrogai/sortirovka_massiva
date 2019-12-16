4bbf6947e2d0dc3cf7d60d387c414234405fd1c1
b302-08 <mihanyka@mail.ru>
2019-12-16 11:12:20 +0300

    method puzhirka (to bilo vibora)

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include "pch.h"
+#include <conio.h>
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	//зашиваем размерность массива
+	const int size = 10;
+
+	//флаг для выхода из сортировки
+	bool flag = false;
+
+	//создание массива
+	double mass[size];
+
+	cout << "\nВведите " << size << " элементов массива:\n";
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << "A [ " << i << " ]= ";
+		cin >> mass[i];
+	}
+
+	//вывод неотсортированного массива
+	cout << "\n\nВведенный массив:\n\n";
+
+	for (int i = 0; i < size; i++)
+		//setw - установка расстояния между элементами массива в выводу( и именно для него iomanip )
+		cout << setw(4) << mass[i];
+
+	while (!flag)
+	{
+		flag = true;
+
+		for (int i = 0; i < size - 1; i++)
+			//по возрастанию - знак > , по убыванию - знак <
+			if (mass[i] > mass[i + 1])
+			{
+				//выполняем перестановку соседних элементов c помощью функции swap(а то с буфером как-то моветон)
+				swap(mass[i], mass[i + 1]);
+
+				flag = false;
+			}
+	}
+
+	//вывод отсортированного массива
+	cout << "\n\nОтсортированный массив:\n\n";
+
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << mass[i];
+
+	_getch();
+	return 0;
+}
